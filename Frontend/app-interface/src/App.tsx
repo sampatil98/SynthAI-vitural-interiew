7d8e7a8764090b3296280a29f71f29e15859e804
Neelachari N H <neelachari34@gmail.com>
2023-07-28 12:41:39 +0530

    Day-1

---
+++ Frontend/app-interface/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Example from './components/Example';
+
+function App() {
+  return (
+    <div className="App">
+      <Example/>
+    </div>
+  );
+}
+
+export default App;
