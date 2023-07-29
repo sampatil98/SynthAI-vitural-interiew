0fcc47beab989a3109df26bfdc9f4aeedf031522
tejareddy9133 <reddyvaritejeshkumarreddy@gmail.com>
2023-07-29 08:42:46 +0530

    sidebar and text bar

--- Frontend/app-interface/src/App.tsx
+++ Frontend/app-interface/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Example from './components/Example';
+
 import Interview from './components/Interview';
 import Sidebar from './components/Sidebar';
 
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
     
-     <Sidebar />
+    
     </div>
   );
 }
