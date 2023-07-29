a9514cdada9a6d61349b7248e7768440104048af
venkat88676 <venkat88676@gmail.com>
2023-07-29 17:09:57 +0530

    some updates

---
+++ Backend/db.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+let connection = mongoose.connect(process.env.mongoURL)
+
+module.exports={connection}
--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,19 @@
 const express=require("express");
 const {openai}=require("./config")
+const {qnaRoute}=require("./routes/qnaRoute")
+const {connection}= require("./db")
 const app=express();
 const cors = require('cors')
 app.use(express.json());
 app.use(cors());
 
+app.use("/qna",qnaRoute)
 
-app.post("/",async (req,res)=>{
+
+app.post("/submit-ans",async (req,res)=>{
     try {
-        const {prompt}=req.body
-        let data= await callChatGPT(prompt);
-
+        const {prompt,studentAnswer}=req.body;
+        let data= await callChatGPT(prompt,studentAnswer);
         res.status(200).send({
             isError:false,
             data:data
@@ -22,14 +25,48 @@ app.post("/",async (req,res)=>{
             error:"internal server error"
         });
     }
-})
+});
+
+app.get('/getQuestion', async (req, res) => {
+    try {
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `ask 1 question on nodejs `,
+        max_tokens: 3000
+      });
+  
+      const question = response.data.choices[0].text.trim();
+      res.json({ question });
+    } catch (error) {
+      console.error('Error fetching question:', error.message);
+      res.status(500).json({ error: 'Server error' });
+    }
+  });
+
+  app.post('/feed-back', async (req, res) => {
+    try {
+      const {avgScore}=req.body;
+
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: ` In the interview interviewer give score ${avgScore} out of 10 to the candidate Now interviewer want to gave feedback to the candidate so please generate feedback`,
+        max_tokens: 3000
+      });
+  
+      const question = response.data.choices[0].text.trim();
+      res.json({ question });
+    } catch (error) {
+      console.error('Error fetching question:', error.message);
+      res.status(500).json({ error: 'Server error' });
+    }
+  });
 
 
-async function callChatGPT(text){
+async function callChatGPT(text,studentAnswer){
     try {
         const completion = await openai.createCompletion({
             model: "text-davinci-003",
-            prompt: text,
+            prompt: `Question:${text}\nStudent Answer: ${studentAnswer}\n compare student Answer with actual answer and give correct answer with Score out of 10. use this format "Score: 0/10 \n feedback:generated feedback " `,
             max_tokens: 3000
         })
         return (completion.data.choices[0].text);
@@ -42,30 +79,9 @@ async function callChatGPT(text){
 
 app.listen(8080,async()=>{
     try {
+        await connection
         console.log("server is running");
     } catch (error) {
         console.log(error);
     }
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
